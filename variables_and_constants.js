75b70aafeaca32241b70259e7f37f2db46be5eba
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-12-10 11:30:51 +0530

    Create variables_and_constants.js

---
+++ variables_and_constants.js
@@ -0,0 +1,12 @@
+/* older versions use var for varible indication and newer version(ES6) use and preder let  */
+
+let x=3; /* x is name of the varible */
+
+console.log(x) //3
+
+// once we assign some values to it then if we need to chage the values of that particular varible then we can simple assign new vlaue to that variable
+
+// like shown bellow
+
+x=5 
+console.log(x) //5
