c4ec22ca1ecb1fb2b0ce4f337c7dfdf462f5b057
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-12-15 10:03:51 +0530

    random number between 1 to 10

---
+++ random_number_generator_between1_to_10.js
@@ -0,0 +1,5 @@
+// constant variable a with random values 
+const a = Math.random() * (10-1)+1
+
+// prints the value which genrated by random
+console.log( ` Random value between 1 and 10 is ${a} ` );
