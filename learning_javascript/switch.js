9b87dec5cdd6d80b47978fce72cc2318ef95a687
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-12-27 10:59:55 +0530

    Update switch.js

--- learning_javascript/switch.js
+++ learning_javascript/switch.js
@@ -1,3 +1,24 @@
+/*
+function day_of_week(day){
+    switch(day){
+        case 1: console.log("Monday");
+        case 2: console.log("Tuesday");
+        case 3: console.log("Wednesday");
+        case 4: console.log("Thursday");
+        case 5: console.log("Friday");
+        case 6: console.log("Saturday");
+        case 7: console.log("Sunday");
+        default: console.log("Invalid day of week!"); 
+    }
+}
+*/
+/*
+Note - on using of console.log while switch case find 
+the case it will start printing from that point to the end point so 
+below shown return will used performed in mozilla console 
+*/
+
+
 function day_of_week(day){
     switch(day){
         case 1: return "Monday";
