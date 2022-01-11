d3937e2069cdeb5fc510e5844d024de8cddcd77c
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2022-01-11 13:32:09 +0530

    Create ternary.js

---
+++ udacity/ternary.js
@@ -0,0 +1,8 @@
+var adult = true;
+var preorder = true;
+
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
+var adult = true;
+var preorder = true;
+
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
