61ceacc33d48dd4e3f8212ce205dcf05086f9ff7
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2022-01-21 10:07:38 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,19 @@
+// importing package
 
+const readline = require("readline")
+
+// creating our interface
+
+const calcInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    //prompt: "DefectiveDetective",
+});
+
+calcInterface.question("Enter the first number: ", (fristInput) => {
+    const firstnumber = Number(fristInput);
+    
+    if(isNaN(firstnumber)){
+        // deal with wrong input
+    }
+})
